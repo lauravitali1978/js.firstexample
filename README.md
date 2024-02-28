9d5ca3399e18a68c4d92423d60c771322c46c7c7
lauravitali1978 <laura@gaw.agency>
2024-02-28 16:05:57 +0100

    completato primo esercizio javascript

---
+++ README.md
@@ -0,0 +1,61 @@
+ESERCITAZIONE DEL 27/02/2024
+
+**Traccia 1:**
+
+Crea 5 variabili contenenti un numero e scrivi un programma in modo da ottenere la somma tra questi numeri e la media.
+
+In console poi mostra la seguente frase: ‘La somma tra i numeri equivale a … e la media equivale a…’ 
+
+
+
+**Traccia 2:**
+
+Crea due variabili contenenti l’anno corrente e l’anno di nascita di una persona. Crea un programma che calcoli:
+- l’eta' della persona
+- quanti anni sono necessari per raggiungere i 100
+
+In console poi mostra la frase “Hai tot anni e ti mancano tot anni per compierne 100“.
+
+
+**Traccia 3:**
+
+Crea due variabili i cui valori verranno scelti dall’utente. Crea un programma che esegua i seguenti calcoli su quei due numeri:
+
+- somma
+- sottrazione
+- moltiplicazione
+- divisione
+- potenza
+
+In seguito in console stampa la frase “Con i numeri da te scelti, i risultati delle varie operazioni sono: somma (tot), sottrazione(tot) ecc ecc“.
+
+HINT
+- Usa parseInt() per trasformare il valore che inserisci nel prompt in un numero → es: parseInt(prompt('Inserisci un numero:'))
+
+
+**Traccia 4:**
+
+Dato il seguente array mescolato e confuso:
+
+`let array_1 = [
+  ['un', 'per', 'incatenarli.'],
+  ['Anello', 'trovarli,'],
+  ['ghermirli', 'e'],
+  ['gondor', 'mark'],
+];
+
+let array_2 = [
+  [['trovarli,']],
+  ['tu,', 'sciocchi'],
+  ['tu,', 'sciocchi', ['padron', 'Sauron']],
+  ['nel', ['fuggite', 'gandalf']],
+  [['domarli,', 'passare'], 'buio']
+];`
+
+Stampa a schermo la frase: "Un Anello per domarli, un Anello per trovarli, un Anello per ghermirli e nel buio incatenarli.“
+
+HINT
+- Non tutte le parole sono necessarie per la frase;
+- Occhio agli array annidati
+
+E DOPO QUESTO QUARTO ESERCIZIO VOGLIO MORIRE!!!!!!!!!!
---
+++ main.js
@@ -0,0 +1,118 @@
+//traccia 1
+/*
+let a = 2;
+let b = 4;
+let c = 8;
+let d = 10;
+let e = 100;
+
+let result = a+b+c+d+e;
+console.log (result);
+
+let media = result / 5;
+console.log ('La somma tra i numeri equivale a ' + result + ' e la media equivale a ' + media);
+*/
+//traccia 2
+/*
+let f = 2024;
+let g = 1978;
+
+let age = f - g;
+console.log(age);
+
+let aspirancy = 100;
+let differenceAge = aspirancy - age;
+//.log(aspirancy - age);
+console.log('Hai ' + age + ' anni e ti mancano ' + differenceAge + ' anni per compierne 100.');
+*/
+
+//traccia 3 seconda prova
+/*
+//addizione
+
+let numb = prompt();
+let numb1 = prompt();
+alert(Number(numb) + Number(numb1));
+console.log('la somma dei numeri scelti è ' + (Number(numb) + Number(numb1)));
+
+//sottrazione
+
+let numb2=prompt();
+let numb3=prompt()
+alert(Number(numb2) - Number(numb3));
+console.log('la differenza tra i numeri è ' + (Number(numb2) - Number(numb3)));
+
+//moltiplicazione
+
+let apple=prompt();
+let boys=prompt();
+alert(Number(apple) * Number(boys));
+console.log('il prodotto tra i numeri è ' + (Number(apple) * Number(boys)) + ' mele');
+
+
+//divisione
+
+let num5=prompt();
+let num6=prompt();
+alert(Number(num5) / Number(num6));
+console.log('il quoziente è pari a ' + (Number(num5) / Number(num6)));
+
+
+//potenza
+
+let num7=prompt();
+let num8=prompt();
+alert(Number(num7) ** Number(num8));
+console.log('l elevamento a potenza di questo numero è  ' + (Number(num7) ** Number(num8)));
+
+
+let somma= 10;
+let sottrazione= 20;
+let moltiplicazione= 15;
+let divisione= 2;
+let potenza= 9;
+
+console.log('Con i numeri da te scelti, i risultati delle varie operazioni sono: ' + ' la somma ' +somma +' la sottrazione ' + sottrazione + ' il prodotto ' + moltiplicazione + ' il quoziente ' + divisione + ' l elevamento a potenza ' + potenza );
+
+*/
+
+//traccia  4
+
+// "Un Anello per domarli, un Anello per trovarli, un Anello per ghermirli e nel buio incatenarli.“
+
+let array_1=[['un','per','incatenarli.'],
+['Anello','trovarli,'],
+['ghermirli','e'],
+['gondor','mark'],
+];
+
+let array_2=[
+[['trovarli,']],
+['tu,',['sciocchi'],['padron','Sauron']],
+['nel',['fuggite','Gandalf']],
+[['domarli,','passare'],'buio']
+];
+
+/*
+console.log(array_1);
+ console.log(array_1[0]);
+ console.log(array_1[1]);
+ console.log(array_1[2]);
+ console.log(array_1[3]);
+
+console.log(array_2);
+    console.log(array_2[0]);
+    console.log(array_2[1]);
+    console.log(array_2[2]);
+    console.log(array_2[3]);
+*/
+
+let fraseCompleta= `${array_1[0][0]} ${array_1[1][0]} ${array_1[0][1]} ${array_2[3][0][0]} ${array_1[0][0] } ${array_1[1][0]} ${array_1[0][1]} ${array_1[1][1]} ${array_1[0][0]} ${array_1[1][0]} ${array_1[0][1]} ${array_1[2][0]} ${array_1[2][1]} ${array_2[2][0]} ${array_2[3][1]} ${array_1[0][2]}`;
+
+console.log(fraseCompleta);
+
+
+
+
+
+
